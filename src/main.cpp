59873fac53fbe87f4e4e9777011b1b8d88219ce6
LisaKirill <lisovets2008@gmail.com>
2026-05-29 16:18:03 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,41 @@
 // Copyright 2022 NNTU-CS
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+  std::srand(time(nullptr));
+  std::cout << "n\tfalse\ttrue\trandom" << std::endl;
+  for (int n = 2; n <= 100; n++) {
+    int F;
+    int T;
+    int R;
+    {
+      Train train;
+      for (int i = 0; i < n; i++)
+        train.addCar(false);
+      train.getLength();
+      F = train.getOpCount();
+    }
+    {
+      Train train;
+      for (int i = 0; i < n; i++)
+        train.addCar(true);
 
-  while (count--)
-    train.addCar(false);
+      train.getLength();
+      T = train.getOpCount();
+    }
+    {
+      Train train;
+      for (int i = 0; i < n; i++)
+        train.addCar(std::rand() % 2);
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+      train.getLength();
+      R = train.getOpCount();
+    }
+    std::cout << n << "\t" << F << "\t" << T << "\t" << R
+              << std::endl;
+  }
   return 0;
 }
