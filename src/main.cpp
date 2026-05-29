fa5fca1db240d65a84eebf926b8034dfb0d70f7d
LisaKirill <lisovets2008@gmail.com>
2026-05-29 17:06:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,69 @@
 // Copyright 2022 NNTU-CS
-#include <cstdlib>
-#include <ctime>
 #include <iostream>
+#include <random>
+#include <fstream>
+#include <iomanip>
 #include "train.h"
 
+using namespace std;
+
+static Train buildTrain(int n, int mode, mt19937& gen) {
+    Train train;
+    uniform_int_distribution<int> bit(0, 1);
+
+    for (int i = 0; i < n; ++i) {
+        bool light = false;
+
+        if (mode == 0) {
+            light = false;
+        } else if (mode == 1) {
+            light = true;
+        } else {
+            light = bit(gen);
+        }
+
+        train.addCar(light);
+    }
+
+    return train;
+}
+
 int main() {
-  std::srand(time(nullptr));
-  std::cout << "n\tfalse\ttrue\trandom"<<"\n";
-  for (int n = 2; n <= 100; n++) {
-    int F;
-    int T;
-    int R;
-    {
-      Train train;
-      for (int i = 0; i < n; i++)
-        train.addCar(false);
-      train.getLength();
-      F = train.getOpCount();
-    }
-    {
-      Train train;
-      for (int i = 0; i < n; i++)
-        train.addCar(true);
+    mt19937 gen(random_device{}());
 
-      train.getLength();
-      T = train.getOpCount();
-    }
-    {
-      Train train;
-      for (int i = 0; i < n; i++)
-        train.addCar(std::rand() % 2);
+    ofstream out("result/experiment.csv");
+    out << "n,all_off,all_on,random\n";
 
-      train.getLength();
-      R = train.getOpCount();
+    cout << setw(6) << "n"
+         << setw(12) << "off"
+         << setw(12) << "on"
+         << setw(12) << "rand" << '\n';
+
+    for (int n = 2; n <= 60; ++n) {
+        Train tOff = buildTrain(n, 0, gen);
+        tOff.getLength();
+        int opOff = tOff.getOpCount();
+
+        Train tOn = buildTrain(n, 1, gen);
+        tOn.getLength();
+        int opOn = tOn.getOpCount();
+
+        long long sumRand = 0;
+        const int trials = 50;
+        for (int i = 0; i < trials; ++i) {
+            Train tRand = buildTrain(n, 2, gen);
+            tRand.getLength();
+            sumRand += tRand.getOpCount();
+        }
+        int opRand = static_cast<int>(sumRand / trials);
+
+        out << n << "," << opOff << "," << opOn << "," << opRand << "\n";
+
+        cout << setw(6) << n
+             << setw(12) << opOff
+             << setw(12) << opOn
+             << setw(12) << opRand << '\n';
     }
-    std::cout << n << "\t" << F << "\t" << T << "\t" << R<<"\n";
-  }
-  return 0;
+
+    return 0;
 }
